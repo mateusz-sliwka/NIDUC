e2d2c6d8a72e4a0bad653f27b685f4957e7b6430
trikam2111 <49413158+trikam2111@users.noreply.github.com>
2019-05-05 19:43:31 +0200

    Update scrambler.py

--- scrambler.py
+++ scrambler.py
@@ -3,50 +3,31 @@ from Signal import Signal
 
 
 def scramble(signal, algorythm):
-    print("\n =====SCRAMBLOWANIE SYGNALU====")
     scrambledsignal = Signal(signal)
+    print("\n =====SCRAMBLOWANIE SYGNALU====")
+    print("Sygnal przed scramblingiem:" + signal)
+    print (scrambledsignal.voltage)
+    print("Sygnal scramblowany algorytmem:"+algorythm)
     if algorythm == "B8ZS":
         i = 0
         while i< (len(scrambledsignal.signal)-7):
             if zeros(scrambledsignal,i,8):
                 if i == 0:
-                    if scrambledsignal.voltage[i] == 'H':
-                        scrambledsignal.signal[i + 3] = '1'
-                        scrambledsignal.voltage[i + 3] = 'H'
-                        scrambledsignal.signal[i + 4] = '1'
-                        scrambledsignal.voltage[i + 4] = 'L'
-                        scrambledsignal.signal[i + 6] = '1'
-                        scrambledsignal.voltage[i + 6] = 'L'
-                        scrambledsignal.signal[i + 7] = '1'
-                        scrambledsignal.voltage[i + 7] = 'H'
-                    else:
-                        scrambledsignal.signal[i + 3] = '1'
-                        scrambledsignal.voltage[i + 3] = 'L'
-                        scrambledsignal.signal[i + 4] = '1'
-                        scrambledsignal.voltage[i + 4] = 'H'
-                        scrambledsignal.signal[i + 6] = '1'
-                        scrambledsignal.voltage[i + 6] = 'H'
-                        scrambledsignal.signal[i + 7] = '1'
-                        scrambledsignal.voltage[i + 7] = 'L'
+                        scrambledsignal.voltage[i + 3] = 'V'
+                        scrambledsignal.voltage[i + 4] = 'B'
+                        scrambledsignal.voltage[i + 6] = 'B'
+                        scrambledsignal.voltage[i + 7] = 'V'
                 else:
                     if scrambledsignal.voltage[i - 1] == 'H':
-                        scrambledsignal.signal[i + 3] = '1'
-                        scrambledsignal.voltage[i + 3] = 'H'
-                        scrambledsignal.signal[i + 4] = '1'
-                        scrambledsignal.voltage[i + 4] = 'L'
-                        scrambledsignal.signal[i + 6] = '1'
-                        scrambledsignal.voltage[i + 6] = 'L'
-                        scrambledsignal.signal[i + 7] = '1'
-                        scrambledsignal.voltage[i + 7] = 'H'
-                    else:
-                        scrambledsignal.signal[i + 3] = '1'
-                        scrambledsignal.voltage[i + 3] = 'L'
-                        scrambledsignal.signal[i + 4] = '1'
-                        scrambledsignal.voltage[i + 4] = 'H'
-                        scrambledsignal.signal[i + 6] = '1'
-                        scrambledsignal.voltage[i + 6] = 'H'
-                        scrambledsignal.signal[i + 7] = '1'
-                        scrambledsignal.voltage[i + 7] = 'L'
+                        scrambledsignal.voltage[i + 3] = 'V'
+                        scrambledsignal.voltage[i + 4] = 'B'
+                        scrambledsignal.voltage[i + 6] = 'B'
+                        scrambledsignal.voltage[i + 7] = 'V'
+                    elif scrambledsignal.voltage[i-1] == 'L':
+                        scrambledsignal.voltage[i + 3] = 'V'
+                        scrambledsignal.voltage[i + 4] = 'B'
+                        scrambledsignal.voltage[i + 6] = 'B'
+                        scrambledsignal.voltage[i + 7] = 'V'
             i += 1
     else:
         i = 0
@@ -55,31 +36,15 @@ def scramble(signal, algorythm):
             if scrambledsignal.signal[i] == '1':
                 j+=1
             if zeros(scrambledsignal,i,4):
-                if scrambledsignal.voltage[i-1] == 'H' and j%2 == 0:
-                    scrambledsignal.signal[i] = '1'
-                    scrambledsignal.voltage[i] = 'L'
-                    scrambledsignal.signal[i+3] = '1'
-                    scrambledsignal.voltage[i+3] = 'L'
+                if (scrambledsignal.voltage[i-1] == 'H' or scrambledsignal.voltage[i-1] == 'L') and j%2 == 0:
+                    scrambledsignal.voltage[i] = 'B'
+                    scrambledsignal.voltage[i+3] = 'V'
                     j = 0
-                elif scrambledsignal.voltage[i-1] == 'H' and j%2 == 1:
-                    scrambledsignal.signal[i+3] = '1'
-                    scrambledsignal.voltage[i+3] = 'H'
+                elif (scrambledsignal.voltage[i-1] == 'H' or scrambledsignal.voltage[i-1] == 'L') and j%2 == 1:
+                    scrambledsignal.voltage[i+3] = 'V'
                     j = 0
-                elif scrambledsignal.voltage[i-1] == 'L' and j%2 == 0:
-                    scrambledsignal.signal[i] = '1'
-                    scrambledsignal.voltage[i] = 'H'
-                    scrambledsignal.signal[i+3] = '1'
-                    scrambledsignal.voltage[i+3] = 'H'
-                    j = 0
-                elif scrambledsignal.voltage[i-1] == 'L' and j%2 == 1:
-                    scrambledsignal.signal[i+3] = '1'
-                    scrambledsignal.voltage[i+3] = 'L'
-                    j = 0
-
             i+=1
-    print("Sygnal przed scramblingiem:" + signal)
-    print("Sygnal scramblowany algorytmem:"+algorythm)
-    print("Sygnal po scramblingu:"+scrambledsignal.signal)
+    print("Sygnal po scramblingu:"+ ''.join(str(item) for item in scrambledsignal.signal))
     print (scrambledsignal.voltage)
     print("=====================")
     return scrambledsignal
@@ -88,4 +53,3 @@ def zeros(scrambledsignal,i,how_many):
         if item == '1':
             return False
     return True
-scramble("1100000000","B8ZS")
