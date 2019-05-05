af39662a49e3357c35c50dc01cc5d8761fe42713
trikam2111 <49413158+trikam2111@users.noreply.github.com>
2019-05-05 19:42:53 +0200

    Update Signal.py

--- Signal.py
+++ Signal.py
@@ -1,16 +1,14 @@
-import array
-
 class Signal:
     def __init__(self, signal):
-        self.signal = signal
-        self.voltage = array.array('B', ['Z' * len(signal)])
+        self.signal = list(signal)
+        self.voltage = ['Z'] * len(signal)
         i = 0
-        while i<len(self.signal):
-            if self.voltage[i] == '1' and i == 0:
+        while i<len(signal):
+            if signal[i] == '1' and i == 0:
                 self.voltage[i]='H'
             else:
-                if self.voltage[i-1] == '1' and self.voltage[i-1] == 'H':
+                if signal[i] == '1' and self.voltage[i-1] == 'H':
                     self.voltage[i] = 'L'
-                elif self.voltage[i-1] =='1':
+                elif self.signal[i] =='1':
                     self.voltage[i] = 'H'
-        i+=1
+            i+=1
