98d63c4891adf1873eadfdb091e52c7067890389
trikam2111 <49413158+trikam2111@users.noreply.github.com>
2019-05-28 20:10:05 +0200

    Update Signal.py

--- Signal.py
+++ Signal.py
@@ -5,12 +5,11 @@ class Signal:
         # Z- Napiece zerowe, H- Napiecie wysokie, L- napiecie niskie
         self.voltage = ['Z'] * len(signal)
         i = 0
-        while i<len(signal):
-            if signal[i] == '1' and i == 0:
+        while i<len(self.signal):
+            if self.signal[i] == 1 and i == 0:
                 self.voltage[i]='H'
-            else:
-                if signal[i] == '1' and self.voltage[i-1] == 'H':
-                    self.voltage[i] = 'L'
-                elif self.signal[i] =='1':
-                    self.voltage[i] = 'H'
+            elif self.signal[i] == 1 and self.voltage[i-1] == 'H':
+                self.voltage[i] = 'L'
+            elif self.signal[i] == 1:
+                self.voltage[i] = 'H'
             i+=1
