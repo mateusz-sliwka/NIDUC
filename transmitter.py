a95142287a0913002b828b14fbd484e1d947e662
trikam2111 <49413158+trikam2111@users.noreply.github.com>
2019-06-03 22:26:56 +0200

    Update transmitter.py

--- transmitter.py
+++ transmitter.py
@@ -1,13 +1,13 @@
 import scrambler
-import receiver
 import descrambler
 import distrupter
-
-
+import receiver
 class transmitter:
     def __init__(self, signal, algorythm):
         pure_disrupted = distrupter.distruption2(signal) #zaklocenie sygnalu wysylanego bez scramblera
         scrambled = scrambler.scramble(signal, algorythm) #zescramblowanie sygnalu
+        tab1 = list(receiver.signalhistogram(scrambled).keys())
+        tab2 = list(receiver.signalhistogram(scrambled).values())
         scrambled_disrupted = distrupter.distruption(scrambled) #zaklocenie sygnalu wysylanego ze scramblerem
         descrambled = descrambler.descramble(scrambled_disrupted, algorythm) #odsramblowanie sygnalu zakloconego wysylanego ze scramblerem
-        receiver.receiver(signal, pure_disrupted, descrambled, algorythm,scrambled, scrambled_disrupted) #przekazanie sygnalow do programu odbiorczego
+        receiver.receiver(signal, pure_disrupted, descrambled, algorythm,scrambled, scrambled_disrupted,tab1,tab2) #przekazanie sygnalow do programu odbiorczego
