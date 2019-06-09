b1b4b6e373905da9c3ed9746fd6997373b2ceb09
trikam2111 <49413158+trikam2111@users.noreply.github.com>
2019-06-09 19:21:39 +0200

    Update distrupter.py

--- distrupter.py
+++ distrupter.py
@@ -15,7 +15,7 @@ def distruption(signal):  # metoda zaklocajaca sygnal
     i = 0
     while i < len(distruptedsignal.signal) - 1:
         b_e.append(i)
-        while distruptedsignal.signal[i] == distruptedsignal.signal[i + 1] and distruptedsignal.voltage[i] != 'V' and distruptedsignal.voltage[i] != 'B':
+        while distruptedsignal.signal[i] == distruptedsignal.signal[i + 1] and distruptedsignal.voltage[i] == 'Z':
             i += 1
             if i > len(distruptedsignal.signal) - 2:
                 break
@@ -63,7 +63,7 @@ def distruption2(signal):  # metoda zaklocajaca sygnal
         i = 0
         while i < len(distruptedsignal)- 1:
             b_e.append(i)
-            while distruptedsignal[i] == distruptedsignal[i+1]:
+            while distruptedsignal[i] == distruptedsignal[i+1] and distruptedsignal[i] == 0:
                     i += 1
                     if i > len(distruptedsignal) - 2:
                         break
