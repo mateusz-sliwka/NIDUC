fa95953913684f2564dfb063d1b7a56375c2dadd
Mateusz Śliwka <kontakt@mateuszsliwka.pl>
2019-06-10 11:20:16 +0200

    distrupter fixing

--- transmitter.py
+++ transmitter.py
@@ -8,6 +8,6 @@ class transmitter:
         scrambled = scrambler.scramble(signal, algorythm) #zescramblowanie sygnalu
         tab1 = list(receiver.signalhistogram(scrambled).keys())
         tab2 = list(receiver.signalhistogram(scrambled).values())
-        scrambled_disrupted = distrupter.distruption3(scrambled,algorythm) #zaklocenie sygnalu wysylanego ze scramblerem
+        scrambled_disrupted = distrupter.distruption(scrambled,algorythm) #zaklocenie sygnalu wysylanego ze scramblerem
         descrambled = descrambler.descramble(scrambled_disrupted, algorythm) #odsramblowanie sygnalu zakloconego wysylanego ze scramblerem
         receiver.receiver(signal, pure_disrupted, descrambled, algorythm,scrambled, scrambled_disrupted,tab1,tab2) #przekazanie sygnalow do programu odbiorczego
