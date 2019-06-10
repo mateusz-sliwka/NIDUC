04a197f4913951075f29c70e17b7f7178a8933a2
Mateusz Śliwka <kontakt@mateuszsliwka.pl>
2019-06-10 23:04:57 +0200

    update

--- distrupter.py
+++ distrupter.py
@@ -1,11 +1,11 @@
 import math
 import random
-from Signal import Signal
 
-def distruption(signal,algorythm):  # metoda zaklocajaca sygnal
-    print("\n =====ZAKLOCANIE SYGNALU====")
-    print("Sygnal przed zakoceniem:" + ''.join(str(item) for item in signal.signal))
-    #Początkowe prawdopodobienstwo p
+
+def distruption(signal, algorythm):  # metoda zaklocajaca sygnal
+    print("\n =====ZAKLOCANIE SCRAMBLOWANEGO SYGNALU====")
+
+    # Początkowe prawdopodobienstwo p
     p = 0.0414
     distruptedsignal = signal
     # Lista ktora bedzie zawierac indeksy poczatku oraz konca ciagu tych samych znakow
@@ -15,33 +15,37 @@ def distruption(signal,algorythm):  # metoda zaklocajaca sygnal
     i = 0
     while i < len(distruptedsignal.signal) - 1:
         b_e.append(i)
-        while distruptedsignal.signal[i] == distruptedsignal.signal[i + 1] and distruptedsignal.voltage[i] == 'Z':
+        while distruptedsignal.voltage[i] == distruptedsignal.voltage[i + 1] and distruptedsignal.voltage[i] == 'Z':
             i += 1
-            if i >=len(distruptedsignal.signal) - 1:
+            if i >= len(distruptedsignal.signal) - 1:
                 break
         b_e.append(i)
-        print(b_e)
-        stala=3 #zmienna mowiaca o tym jak dlugi ma byc ciag tych samych zankow zeby wystapilo zaklocenie
-        if(algorythm=="B8ZS"):
-            stala=4
+
+        stala = 3  # zmienna mowiaca o tym jak dlugi ma byc ciag tych samych zankow zeby wystapilo zaklocenie
+        if (algorythm == "B8ZS"):
+            stala = 4
         # Zaklocanie odbywa sie jezeli pojawi sie ciag powyzej 4 takich samych znakow
 
-        ilosc = b_e[1]-b_e[0]+1    #ilosc bitow takkich samych w danym ciagu np dla 1111 ilosc =4
-        if ilosc >= stala:  #jezeli ilosc jest wieksza niz stala do zaklocenia
+        ilosc = b_e[1] - b_e[0] + 1  # ilosc bitow takkich samych w danym ciagu np dla 1111 ilosc =4
+
+        if ilosc >= stala:  # jezeli ilosc jest wieksza niz stala do zaklocenia
+
             j = 0
-            while j <= ilosc - stala:  #iterujemy po roznicy ilosci i stalej, nie rozumiem tej petli, czemu po prostu nie j=ilosc-stala?
+            while j <= ilosc - stala:  # iterujemy po roznicy ilosci i stalej
                 j += 1
             p = p + j * 0.000012
             p = math.ceil(p * ilosc)
             j = 0
             while j < p:
-                n_b.append(random.randint(b_e[0], b_e[1]))  #losowanie bitow do zaklocenia, czy moze wylosowac kilka razy ten sam?
-                j += 1 #j bedzie mialo dokladnie taka wartosc jak len(n_b)
-            for j in range(len(n_b)):   #tutaj j nie jest 0, j ma wartosc od iteracji z while wyzej, to dobrze?
+                n_b.append(random.randint(b_e[0], b_e[
+                    1]))
+                j += 1
+            for j in range(len(
+                    n_b)):
                 if distruptedsignal.signal[n_b[j]] == 1:
                     distruptedsignal.signal[n_b[j]] = 0
                     distruptedsignal.voltage[n_b[j]] = 'Z'
-                elif distruptedsignal.voltage[j-1] == 'H' and distruptedsignal.signal == 0:
+                elif distruptedsignal.voltage[j - 1] == 'H' and distruptedsignal.signal == 0:
                     distruptedsignal.signal[n_b[j]] = 1
                     distruptedsignal.voltage[n_b[j]] = 'L'
                 elif distruptedsignal.signal[n_b[j]] == 0:
@@ -52,53 +56,54 @@ def distruption(signal,algorythm):  # metoda zaklocajaca sygnal
         p = 0.0414
         i += 1
     print("Sygnal po zakloceniu:" + ''.join(str(item) for item in distruptedsignal.signal))
+    print(distruptedsignal.voltage)
     print("=====================")
     return distruptedsignal
 
 
-def distruption2(signal,algorythm):  # metoda zaklocajaca sygnal
+def distruption2(signal, algorythm):  # metoda zaklocajaca sygnal
     # Początkowe prawdopodobienstwo p
-        p = 0.0414
-        distruptedsignal = []
-        for item in signal:
-            distruptedsignal.append(item)
-        # Lista ktora bedzie zawierac indeksy poczatku oraz konca ciagu tych samych znakow
-        b_e = []
-        # Lista ktora bedzie zawierac losowo miejsca zaklocen sygnalu
-        n_b = []
-        i = 0
-        while i < len(distruptedsignal)- 1:
-            b_e.append(i)
-            while distruptedsignal[i] == distruptedsignal[i+1] and distruptedsignal[i] == 0:
-                    i += 1
-                    if i > len(distruptedsignal) - 2:
-                        break
-            b_e.append(i)
-            stala = 3
-            if (algorythm == "B8ZS"):
-                stala = 4
-            # Zaklocanie odbywa sie jezeli pojawi sie ciag powyzej 3 takich samych znakow
-            if (b_e[1]-b_e[0]+1) >= stala:
-                j = 0
-                while j < (b_e[1]-b_e[0]+1) - stala:
-                    j += 1
-                p = p + j * 0.000012
-                p = math.ceil(p * (b_e[1] - b_e[0]+1))
-                j = 0
-                while j < p:
-                    n_b.append(random.randint(b_e[0], b_e[1]))
-                    j+=1
-                for j in range(len(n_b)):
-                    if distruptedsignal[n_b[j]] == 1:
-                        distruptedsignal[n_b[j]] = 0
-                    else:
-                        distruptedsignal[n_b[j]] = 1
-            b_e.clear()
-            n_b.clear()
-            p = 0.0414
+    p = 0.0414
+    distruptedsignal = []
+    for item in signal:
+        distruptedsignal.append(item)
+    # Lista ktora bedzie zawierac indeksy poczatku oraz konca ciagu tych samych znakow
+    b_e = []
+    # Lista ktora bedzie zawierac losowo miejsca zaklocen sygnalu
+    n_b = []
+    i = 0
+    while i < len(distruptedsignal) - 1:
+        b_e.append(i)
+        while distruptedsignal[i] == distruptedsignal[i + 1] and distruptedsignal[i] == 0:
             i += 1
-        print("\n =====ZAKLOCANIE SYGNALU====")
-        print("Sygnal przed zakoceniem:" + ''.join(str(item) for item in signal))
-        print("Sygnal po zaklocenie:" + ''.join(str(item) for item in distruptedsignal))
-        print("=====================")
-        return distruptedsignal
+            if i >= len(distruptedsignal) - 1:
+                break
+        b_e.append(i)
+        stala = 3
+        if (algorythm == "B8ZS"):
+            stala = 4
+        ilosc = b_e[1] - b_e[0] + 1
+        # Zaklocanie odbywa sie jezeli pojawi sie ciag powyzej 3 takich samych znakow
+        if ilosc >= stala:
+            j = 0
+            while j <= ilosc - stala:
+                j += 1
+            p = p + j * 0.000012
+            p = math.ceil(p * ilosc)
+            j = 0
+            while j < p:
+                n_b.append(random.randint(b_e[0], b_e[1]))
+                j += 1
+            for j in range(len(n_b)):
+                if distruptedsignal[n_b[j]] == 1:
+                    distruptedsignal[n_b[j]] = 0
+                else:
+                    distruptedsignal[n_b[j]] = 1
+        b_e.clear()
+        n_b.clear()
+        p = 0.0414
+        i += 1
+    print("\n =====ZAKLOCANIE NIESCRAMBLOWANEGO SYGNALU====")
+    print("Sygnal po zakloceniU:" + ''.join(str(item) for item in distruptedsignal))
+    print("=====================")
+    return distruptedsignal
--- transmitter.py
+++ transmitter.py
@@ -6,16 +6,13 @@ import receiver
 
 class transmitter:
     def __init__(self, signal, algorythm,now):
-        if(algorythm=='AES'):
-            print("AES")
-        else:
-            pure_disrupted = distrupter.distruption2(signal, algorythm)  # zaklocenie sygnalu wysylanego bez scramblera
-            scrambled = scrambler.scramble(signal, algorythm)  # zescramblowanie sygnalu
-            tab1 = list(receiver.signalhistogram(scrambled).keys())
-            tab2 = list(receiver.signalhistogram(scrambled).values())
-            scrambled_disrupted = distrupter.distruption(scrambled,
-                                                         algorythm)  # zaklocenie sygnalu wysylanego ze scramblerem
-            descrambled = descrambler.descramble(scrambled_disrupted,
-                                                 algorythm)  # odsramblowanie sygnalu zakloconego wysylanego ze scramblerem
-            receiver.receiver(signal, pure_disrupted, descrambled, algorythm, scrambled, scrambled_disrupted, tab1,
-                              tab2,now)  # przekazanie sygnalow do programu odbiorczego
+        pure_disrupted = distrupter.distruption2(signal, algorythm)  # zaklocenie sygnalu wysylanego bez scramblera
+        scrambled = scrambler.scramble(signal, algorythm)  # zescramblowanie sygnalu
+        tab1 = list(receiver.signalhistogram(scrambled).keys())
+        tab2 = list(receiver.signalhistogram(scrambled).values())
+        scrambled_disrupted = distrupter.distruption(scrambled,
+                                                     algorythm)  # zaklocenie sygnalu wysylanego ze scramblerem
+        descrambled = descrambler.descramble(scrambled_disrupted,
+                                             algorythm)  # odsramblowanie sygnalu zakloconego wysylanego ze scramblerem
+        receiver.receiver(signal, pure_disrupted, descrambled, algorythm, scrambled, scrambled_disrupted, tab1,
+                          tab2,now)  # przekazanie sygnalow do programu odbiorczego
